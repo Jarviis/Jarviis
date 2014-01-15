6890ba25958aad6830fa81371a8ed6f24606135c
Aggelos Avgerinos <avgerinos@skroutz.gr>
2014-01-15 19:22:40 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jarviis::Application.config.secret_key_base = '4038a2ad3de8d9f65ffbacd32cceb73e636ce944907f13f03a741d544d43e97fe39812ea52e6f897e5930342ef071cb5e7ae771029ae8ad028b9fad4f0c54b1d'
