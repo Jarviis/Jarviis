a0f377146fdba7d91a9c3fc6115fd898635388ea
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2014-01-26 23:43:59 +0200

    Adds close event for modal and removes a useless class

--- app/assets/javascripts/regions/modal.js
+++ app/assets/javascripts/regions/modal.js
@@ -1,6 +1,12 @@
 Jarviis.Regions.Modal = Backbone.Marionette.Region.extend({
   el: "#modal",
+  events: {
+    'click .close': 'exit'
+  },
   initialize: function () {
+  },
+  exit: function () {
+    alert('lalal')
   },
   onShow: function() {
     this.$el.show();
--- app/assets/javascripts/views/CreateIssue.js
+++ app/assets/javascripts/views/CreateIssue.js
@@ -1,3 +1,9 @@
 Jarviis.Views.CreateIssue = Backbone.Marionette.ItemView.extend({
-  template: _.template($('#add-issue-modal').text())
+  template: _.template($('#add-issue-modal').text()),
+  events: {
+    'click .close': 'exit'
+  },
+  exit: function () {
+    Jarviis.modal.close();
+  },
 });
