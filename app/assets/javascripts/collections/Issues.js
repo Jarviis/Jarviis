fc1d9942d953cbef5b8349aff03f6d2637536488
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2014-03-05 19:44:44 +0200

    Backbone code for displaying assigned and reported issues

--- app/assets/javascripts/collections/Issues.js
+++ app/assets/javascripts/collections/Issues.js
@@ -1,4 +1,10 @@
 Jarviis.Collections.Issues = Backbone.Collection.extend({
   model: Jarviis.Models.Issue,
-  url: '/api/'+apiVer+'/issues'
+  url: function() {
+    return '/api/'+apiVer+'/issues'+(this.type ? '?'+this.type+'_username='+this.username : '')
+  },
+  initialize: function (options) {
+    this.type = options.type;
+    this.username = options.username;
+  }
 });
--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,15 +1,20 @@
 Jarviis.addRegions({
   navbar: '#nav',
-  issues: "#issues",
+  assigned: "#assigned",
+  reported: "#reported",
   modal: Jarviis.Regions.Modal
 });
 
 Jarviis.addInitializer(function() {
-  Jarviis.b.issues = new Jarviis.Collections.Issues();
-  Jarviis.b.issues.fetch();
+  Jarviis.b.assigned_issues = new Jarviis.Collections.Issues({type: "assignee", username: current_username});
+  Jarviis.b.assigned_issues.fetch();
+
+  Jarviis.b.reported_issues = new Jarviis.Collections.Issues({type: "reporter", username: current_username});
+  Jarviis.b.reported_issues.fetch();
 
   Jarviis.navbar.attachView(new Jarviis.Views.NavView({el: $(".navbar")}));
-  Jarviis.issues.show(new Jarviis.Views.IssuesView({collection: Jarviis.b.issues}));
+  Jarviis.assigned.show(new Jarviis.Views.IssuesView({collection: Jarviis.b.assigned_issues}));
+  Jarviis.reported.show(new Jarviis.Views.IssuesView({collection: Jarviis.b.reported_issues}));
 });
 
 $(document).ready(function() {
