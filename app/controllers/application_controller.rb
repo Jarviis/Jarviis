fb5bcaef4442696555f51055314cd04ec19e9dd1
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2014-03-06 14:55:40 +0200

    Public pages added.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :authenticate_user!
   before_filter :configure_devise_permitted_parameters, if: :devise_controller?
+
+  layout :determine_layout
+
+  private
+    def determine_layout
+      current_user ? "application" : "landing"
+    end
 
   protected
 
