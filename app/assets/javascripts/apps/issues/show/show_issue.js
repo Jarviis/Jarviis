d94fb3374db2b2ea00684ac4c3a9eb1d4a78922e
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2014-03-08 13:41:54 +0200

    Adding back navigation button, to go back in History API

--- app/assets/javascripts/apps/issues/show/show_issue.js
+++ app/assets/javascripts/apps/issues/show/show_issue.js
@@ -5,5 +5,12 @@ Jarviis.module("Issues.Show", function(Show, Jarviis, Backbone, Marionette, $, _
 
   Show.Issue = Marionette.ItemView.extend({
     template: "#show-issue-template",
+    events: {
+      'click .back': 'goBack'
+    },
+    goBack: function (e) {
+      e.preventDefault();
+      window.history.back();
+    }
   });
 });
