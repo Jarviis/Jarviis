33ef95c154cac92d84366d031797284824fc36e3
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2014-03-16 18:13:07 +0200

    Format dates for human beings in layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,7 @@
 //= require backbone.marionette
 //= require_tree ./vendors
 //= require backbone-syphon
-//= require ./common/regions/modal
+//= require_tree ./common
 //= require dashboard
 //= require_tree ./entities
 //= require_tree ./apps
