7b72e3f530ae70647c3fdd85127426f02323177e
Aggelos Avgerinos <evaggelos.avgerinos@gmail.com>
2014-03-30 01:18:24 +0200

    Fix devise permitted params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_devise_permitted_parameters
-    registration_params = [:name, :email, :password, :password_confirmation]
+    registration_params = [:username, :name, :email, :password, :password_confirmation]
 
     if params[:action] == 'update'
       devise_parameter_sanitizer.for(:account_update) {
