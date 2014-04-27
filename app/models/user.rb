1b997ed3523cda4fb119098932f5b185979be9df
Aggelos Avgerinos <evaggelos.avgerinos@gmail.com>
2014-04-27 19:45:42 +0300

    Adapt User model the new elasticsearch library

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,23 @@
 class User < ActiveRecord::Base
-  include Tire::Model::Search
-  include Tire::Model::Callbacks
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  mapping do
-    indexes :id, type: "integer"
-    indexes :email, boost: 10
-    indexes :name
-    indexes :username, boost: 10
-    indexes :created_at, type: "date"
-    indexes :updated_at, type: "date"
+  index_name "users_#{Rails.env}"
+
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :id, type: "integer"
+      indexes :email, boost: 10
+      indexes :name
+      indexes :username, boost: 10
+      indexes :created_at, type: "date"
+      indexes :updated_at, type: "date"
+    end
   end
 
   has_many :assigned_issues, class_name: "Issue", foreign_key: "assignee_id"
@@ -25,7 +29,7 @@ class User < ActiveRecord::Base
   validates_presence_of   :username
 
   def self.search(params)
-    tire.search do |es|
+    self.__elasticsearch__.search(params[:query]) do |es|
       es.query { string params[:query], default_operator: "AND" } if params[:query].present?
       es.filter :term, username: params[:username] if params[:username]
       es.filter :term, email: params[:username] if params[:username]
