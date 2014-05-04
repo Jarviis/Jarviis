4e51480c60f52ea3578cf8ac301220a33de49c71
Aggelos Avgerinos <evaggelos.avgerinos@gmail.com>
2014-05-05 00:05:49 +0300

    Introduce ElasticsearchQuerySanitizer

---
+++ lib/elasticsearch_query_sanitizer.rb
@@ -0,0 +1,18 @@
+module ElasticsearchQuerySanitizer
+  extend self
+
+  def sanitize(string)
+    replace_punctuation(string)
+    replace_reserved_keywords(string)
+    string.squish
+  end
+
+  def replace_punctuation(string)
+    punctuation = '([\~:\-\\\/\!\.,\|])'
+    string.gsub!(/(^|\s)#{punctuation}+|#{punctuation}+(\s|$)/, ' ')
+  end
+
+  def replace_reserved_keywords(string)
+    string.gsub!(/(\s|^)(AND|OR|NOT)(\s|$)/i, ' ')
+  end
+end
