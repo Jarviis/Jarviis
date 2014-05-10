971b645643143eaf86cfc79212baef16b1de75d4
Aggelos Avgerinos <evaggelos.avgerinos@gmail.com>
2014-05-10 21:55:48 +0300

    Refactor users index action in api controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::UsersController < Api::V1::ApiController
   def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  def search
     @users = User.search(params)
 
     render json: @users
