56e1b57a451ea62960741f7a68ee45088598d70c
Aggelos Avgerinos <evaggelos.avgerinos@gmail.com>
2014-05-11 18:23:42 +0300

    Add users app

---
+++ app/assets/javascripts/apps/users/users_app.js
@@ -0,0 +1,19 @@
+Jarviis.module("Users", function (Users, Jarviis, Backbone, Marionette, $, _) {
+  Users.Router = Marionette.AppRouter.extend({
+    appRoutes: {
+      "": "indexUsers"
+    }
+  });
+
+  var Controller = {
+    indexUsers: function () {
+      Jarviis.Users.Index.Controller.indexUsers();
+    }
+  };
+
+  Jarviis.addInitializer(function () {
+    new Users.Router({
+      controller: Controller
+   });
+  });
+});
