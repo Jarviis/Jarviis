b0945ca1c025f13a5c26e179d7f9b7c5147838a9
Aggelos Avgerinos <evaggelos.avgerinos@gmail.com>
2014-05-24 20:12:19 +0300

    Authorize AdminController

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,18 @@
 class AdminController < ActionController::Base
   protect_from_forgery
   before_action :authenticate_user!
+  before_action :authorize!
   layout "layouts/admin"
+
+  private
+
+  def permission
+    @permission ||= Permission.new(current_user)
+  end
+
+  def authorize!
+    if !permission.allow?
+      redirect_to root_url, alert: "Not authorized"
+    end
+  end
 end
