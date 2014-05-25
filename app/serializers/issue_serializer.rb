5df10e0a4041adbd72ab33f9728151af32094be6
Aggelos Avgerinos <evaggelos.avgerinos@gmail.com>
2014-05-25 13:11:29 +0300

    Update IssueSerializer to support the tree behavior
    Also add some test for the serializer

--- app/serializers/issue_serializer.rb
+++ app/serializers/issue_serializer.rb
@@ -1,6 +1,10 @@
 class IssueSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :state,
-             :due_date, :parent_id, :created_at, :updated_at
+             :due_date, :created_at, :updated_at,
+             :parent
+
   has_one :reporter
   has_one :assignee
+  has_many :children, serializer: ChildrenSerializer, include: true
+  has_one :parent, serializer: ChildrenSerializer, include: true
 end
