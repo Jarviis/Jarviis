eb512f2b2c00b3bf1505aac9086794a648ce5db6
Aggelos Avgerinos <evaggelos.avgerinos@gmail.com>
2014-10-26 21:20:00 +0200

    Handle forgery attacks with null session in api controller

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::ApiController < ActionController::Base
-  protect_from_forgery
-  before_action :authenticate_user!
+  abstract!
+
+  protect_from_forgery with: :null_session
+
+  before_action :authenticate
 
   respond_to :json
 end
