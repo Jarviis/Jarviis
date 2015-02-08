3d67dd239ab08debe23d773db5c97b049a1bee3d
Aggelos Avgerinos <evaggelos.avgerinos@gmail.com>
2015-02-08 22:38:55 +0200

    Add helper methods in Attachment model

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,5 +1,27 @@
 class Attachment < ActiveRecord::Base
+  include ActiveModel::Serializers::JSON
+
   belongs_to :issue
 
   mount_uploader :filename, AttachmentUploader
+
+  validates_presence_of :filename
+
+  def versions
+    image? ? [:thumbnail] : []
+  end
+
+  def image?
+    !!(filename.file.content_type =~ /^image\/*/)
+  end
+
+  def url
+    h = { main: filename.url }
+
+    versions.each do |version|
+      h[version] = filename.url(version)
+    end
+
+    h
+  end
 end
--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -2,4 +2,91 @@ require 'spec_helper'
 
 describe Attachment do
   it { should belong_to(:issue) }
+
+  let(:user) { FactoryGirl.create(:user) }
+  let(:issue) { FactoryGirl.create(:issue, reporter: user) }
+
+  describe "#versions" do
+    context "when the file is an image" do
+      let(:attachment) do
+        FactoryGirl.create(:attachment,
+                          issue: issue,
+                          filename: File.open(Rails.root.join("spec/fixtures/Lenna.png"))
+                          )
+      end
+
+      it "returns the versions of the image" do
+        expect(attachment.versions).to eq([:thumbnail])
+      end
+    end
+
+    context "when the file is not an image" do
+      let(:attachment) do
+        FactoryGirl.create(:attachment,
+                          issue: issue,
+                          filename: File.open(Rails.root.join("app/models/issue.rb"))
+                          )
+      end
+
+      it "returns an empty array" do
+        expect(attachment.versions).to eq([])
+      end
+    end
+  end
+
+  describe "#image?" do
+    context "when the file is an image" do
+      let(:attachment) do
+        FactoryGirl.create(:attachment,
+                          issue: issue,
+                          filename: File.open(Rails.root.join("spec/fixtures/Lenna.png"))
+                          )
+      end
+
+      it "returns true" do
+        expect(attachment.image?).to be_true
+      end
+    end
+
+    context "when the file is not an image" do
+      let(:attachment) do
+        FactoryGirl.create(:attachment,
+                          issue: issue,
+                          filename: File.open(Rails.root.join("app/models/issue.rb"))
+                          )
+      end
+
+      it "returns false" do
+        expect(attachment.image?).to be_false
+      end
+    end
+  end
+
+  describe "#url" do
+    context "when the file is an image" do
+      let(:attachment) do
+        FactoryGirl.create(:attachment,
+                          issue: issue,
+                          filename: File.open(Rails.root.join("spec/fixtures/Lenna.png"))
+                          )
+      end
+
+      it "returns the versions of the image" do
+        expect(attachment.url.keys).to match_array([:main, :thumbnail])
+      end
+    end
+
+    context "when the file is not an image" do
+      let(:attachment) do
+        FactoryGirl.create(:attachment,
+                          issue: issue,
+                          filename: File.open(Rails.root.join("app/models/issue.rb"))
+                          )
+      end
+
+      it "returns only main" do
+        expect(attachment.url.keys).to match_array([:main])
+      end
+    end
+  end
 end
