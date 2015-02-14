7d7205148bbaa8691d69be36019756f8564272ac
Aggelos Avgerinos <evaggelos.avgerinos@gmail.com>
2015-02-14 23:25:40 +0200

    Add team_id to Issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150214185403) do
+ActiveRecord::Schema.define(version: 20150214194110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,11 +58,13 @@ ActiveRecord::Schema.define(version: 20150214185403) do
     t.integer  "parent_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "team_id"
   end
 
   add_index "issues", ["assignee_id"], name: "index_issues_on_assignee_id", using: :btree
   add_index "issues", ["parent_id"], name: "index_issues_on_parent_id", using: :btree
   add_index "issues", ["reporter_id"], name: "index_issues_on_reporter_id", using: :btree
+  add_index "issues", ["team_id"], name: "index_issues_on_team_id", using: :btree
 
   create_table "team_relationships", force: true do |t|
     t.integer  "user_id"
