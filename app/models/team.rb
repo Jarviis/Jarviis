d70fadca397a5ff7f57bc04e608282ee968f7a88
Aggelos Avgerinos <evaggelos.avgerinos@gmail.com>
2015-02-15 17:56:39 +0200

    Associate Sprint with Team

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,11 +1,18 @@
 class Team < ActiveRecord::Base
   has_many :team_relationships
   has_many :users, through: :team_relationships
+  has_many :sprints
 
   before_save :upercase_slug, if: -> { new_record? || slug_changed? }
 
   validates_presence_of :slug
   validates_uniqueness_of :slug
+
+  def active_sprint
+    @active_sprint ||= self.sprints.
+      where("starttime <= :starttime AND endtime < :endtime",
+            { starttime: Time.now, endtime: Time.now }).first
+  end
 
   private
 
